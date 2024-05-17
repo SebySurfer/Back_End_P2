7d85d57eb647c16c2bff8377c2b91963b4112a77
SebySurfer <sebysurfer@gmail.com>
2024-05-17 16:14:32 -0600

    Changed port
    No errors. Only error is "faltan datos"

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,10 @@ const cors = require('cors');
 
 connectDB()
 
-const port = process.env.PORT || 6000
+const port = process.env.PORT || 4000
 
 const app = express()
 
-const corsOptions = {
-    origin: 'http://127.0.0.1:5500/User_Orders.html?name=Sebastian+W.&email=t67ftfhi%40gmail.com&delivery-address=7979rt&contact-number=6r856e#', // Replace this with the actual URL of your frontend
-};
 
 
 app.use(cors());
