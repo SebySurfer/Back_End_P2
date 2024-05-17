76f675e80aaad055c4b2e26b1d1886666864247d
SebySurfer <sebysurfer@gmail.com>
2024-05-17 13:55:53 -0600

    Added CORS
    No postman errors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const colors = require('colors')
 const connectDB = require('./config/db')
 const dotenv = require('dotenv').config()
+const cors = require('cors');
 
 connectDB()
 
@@ -9,6 +10,7 @@ const port = process.env.PORT || 6000
 
 const app = express()
 
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
